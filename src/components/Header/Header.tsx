bd4f787e909c8ddc0fc5b3f3ed5e2fa2740c212c
tatdt622989 <70212cbkj67@gmail.com>
2024-01-04 00:32:30 +0800

    Set up development environment

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function Header() {
+    return (
+        <div></div>
+    )
+}
+
+export default Header
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+
+import { store } from './store/store.ts';
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
