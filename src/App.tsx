83ee5af42876b2aef7375b1ec206a7c954bef583
tatdt622989 <70212cbkj67@gmail.com>
2024-01-05 01:20:38 +0800

    Update header component

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Header />
-    </>
+    <div className='app container'>
+      <div className="row">
+        <Header />
+      </div>
+    </div>
   )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+import UserIcon from "@/assets/user.svg?react";
+import "./Header.scss";
 
 function Header() {
-    return (
-        <div></div>
-    )
+  return (
+    <div className="header d-flex justify-content-between align-items-center">
+      <h1 className="title">GuessAI Canvas</h1>
+      <div className="info d-flex align-items-center">
+        <span className="user-name">6yuwei</span>
+        <div className="user-photo d-flex justify-content-center align-items-center">
+          {/* <img src="" alt="" /> */}
+          <UserIcon className="icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
